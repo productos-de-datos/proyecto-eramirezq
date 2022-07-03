cd201b9775b678e806513b031414b9c9bca91b0b
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-07-03 15:11:38 -0500

    Update pipeline.py

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -20,44 +20,43 @@ from ingest_data import ingest_data
 from transform_data import transform_data
 from compute_daily_prices import compute_daily_prices
 from compute_monthly_prices import compute_monthly_prices
-
-class ingestData(luigi.Task):
-    def output(self):
-        return []
-    
-    def run(self):
-        ingest_data()
-        
-class transformData(luigi.Task):
-    def output(self):
-        return []
-    
-    def run(self):
-        transform_data()
-
-class cleanData(luigi.Task):
-    def output(self):
-        return []
-    
-    def run(self):
-        clean_data()
-        
-class computeDailyPrices(luigi.Task):
-    def output(self):
-        return []
-    
-    def run(self):
-        compute_daily_prices()
-        
-class computeMonthlyPrices(luigi.Task):
-    def output(self):
-        return []
-    
-    def run(self):
-        compute_monthly_prices()
-        
                 
 if __name__ == "__main__":
+    
+    class ingestData(luigi.Task):
+        def output(self):
+            return []
+    
+        def run(self):
+            ingest_data()
+        
+    class transformData(luigi.Task):
+        def output(self):
+            return []
+    
+        def run(self):
+            transform_data()
+
+    class cleanData(luigi.Task):
+        def output(self):
+            return []
+    
+        def run(self):
+            clean_data()
+        
+    class computeDailyPrices(luigi.Task):
+        def output(self):
+            return []
+    
+        def run(self):
+            compute_daily_prices()
+        
+    class computeMonthlyPrices(luigi.Task):
+        def output(self):
+            return []
+    
+        def run(self):
+            compute_monthly_prices()
 
     raise NotImplementedError("Implementar esta función")
 
