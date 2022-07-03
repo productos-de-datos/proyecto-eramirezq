10c1d0396107fc5f2b9367ac8207f51dc1cd6c42
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-07-03 17:43:19 -0500

    Update pipeline.py

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -12,51 +12,53 @@ En luigi llame las funciones que ya creo.
 
 """
 import luigi
+
 from clean_data import clean_data
 
 from ingest_data import ingest_data
 from transform_data import transform_data
 from compute_daily_prices import compute_daily_prices
 from compute_monthly_prices import compute_monthly_prices
+
+class ingestData(luigi.Task):
+    def output(self):
+        return []
+    
+    def run(self):
+        ingest_data()
+        
+class transformData(luigi.Task):
+    def output(self):
+        return []
+    
+    def run(self):
+        transform_data()
+
+class cleanData(luigi.Task):
+    def output(self):
+        return []
+    
+    def run(self):
+        clean_data()
+        
+class computeDailyPrices(luigi.Task):
+    def output(self):
+        return []
+    
+    def run(self):
+        compute_daily_prices()
+        
+class computeMonthlyPrices(luigi.Task):
+    def output(self):
+        return []
+    
+    def run(self):
+        compute_monthly_prices()
+        
                 
 if __name__ == "__main__":
-    
-    class ingestData(luigi.Task):
-        def output(self):
-            return []
-    
-        def run(self):
-            ingest_data()
-        
-    class transformData(luigi.Task):
-        def output(self):
-            return []
-    
-        def run(self):
-            transform_data()
 
-    class cleanData(luigi.Task):
-        def output(self):
-            return []
-    
-        def run(self):
-            clean_data()
-        
-    class computeDailyPrices(luigi.Task):
-        def output(self):
-            return []
-    
-        def run(self):
-            compute_daily_prices()
-        
-    class computeMonthlyPrices(luigi.Task):
-        def output(self):
-            return []
-    
-        def run(self):
-            compute_monthly_prices()
-
-    #raise NotImplementedError("Implementar esta función")
+    raise NotImplementedError("Implementar esta función")
 
 if __name__ == "__main__":
     import doctest
