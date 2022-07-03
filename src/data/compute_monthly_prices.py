692af80380fb4cfee59730ecda1393a939e914e1
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-07-03 14:47:30 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -12,10 +12,23 @@ def compute_monthly_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    
+    import pandas as pd
+    import datetime as dt
+    df = pd.read_csv('data_lake/cleansed/precios-horarios.csv',sep=',',thousands=None, decimal=',', header=0)
+    
+    df['fecha']=pd.to_datetime(df['fecha'], format="%Y/%m")
+    
+    df['fecha']=df['fecha'].dt.strftime('%Y-%m')
+    promedio_precios_mes=df.groupby('fecha')['precio'].mean()
+    
+    promedio_precios_mes.to_csv('data_lake/business/precios-mensuales.csv', index=True, decimal=',')
+    
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
+    compute_monthly_prices()
     import doctest
 
     doctest.testmod()
