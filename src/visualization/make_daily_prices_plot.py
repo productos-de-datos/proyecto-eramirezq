231f185eecd0e8e80e72a54688328f1c0e83ac5f
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-07-04 01:19:08 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -7,10 +7,21 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    
+    df = pd.read_csv('./data_lake/business/precios-diarios.csv', decimal=',')
+    df.fecha = pd.to_datetime(df.fecha, errors='coerce')
+    df = df.set_index('fecha')  
+    _= df.plot( color='tab:blue', figsize=(17,6), marker='.')
+    plt.title('Precio energia promedio diario ')
+    plt.savefig("data_lake/business/reports/figures/daily_prices.png")    
+    
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
+    make_daily_prices_plot()
     import doctest
 
     doctest.testmod()
