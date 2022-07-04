056c84a45b70912f5bcce7fb5c9b3e3fb7d34520
Esteban Ramirez <101228857+eramirezq@users.noreply.github.com>
2022-07-04 17:39:56 -0500

    Update compute_daily_prices.py

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -1,3 +1,14 @@
+"""
+Modulo computar promedio precios por día.
+
+----------------------------------------------
+
+"""
+
+
+import pandas as pd
+import pandas._testing as tm
+
 def compute_daily_prices():
     """Compute los precios promedios diarios.
 
@@ -9,18 +20,36 @@ def compute_daily_prices():
 
     * precio: precio promedio diario de la electricidad en la bolsa nacional
 
-
-
     """
-    import pandas as pd
+    
     df = pd.read_csv('data_lake/cleansed/precios-horarios.csv',sep=',',thousands=None, decimal=',', header=0)
-    promedio_precios_dia=df.groupby('fecha')['precio'].mean()
+    promedio_precios_dia=average_daily_prices(df)
     
     promedio_precios_dia.to_csv('data_lake/business/precios-diarios.csv', index=True, decimal=',')
-    
-    
+        
     #raise NotImplementedError("Implementar esta función")
+    
+    
+def average_daily_prices(data):
+    
+    prom=data.groupby('fecha')['precio'].mean()
+    
+    return prom
 
+def test_average_daily_prices():
+    df = pd.read_csv('data_lake/cleansed/precios-horarios.csv',sep=',',thousands=None, decimal=',', header=0)
+    result=average_daily_prices(df).head()
+    expected=pd.Series(
+          {
+                "1995-07-20": 1.409435, 
+                "1995-07-21": 4.924333, 
+                "1995-07-22": 1.269500, 
+                "1995-07-23": 0.953083,
+                "1995-07-24": 4.305917,
+            }, index=pd.Series(["1995-07-20", "1995-07-21", "995-07-22", "1995-07-23", "1995-07-24" ], name="fecha"), 
+          name="precio"
+         )
+    tm.assert_frame_equal(result, expected)
 
 if __name__ == "__main__":
     
